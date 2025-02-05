c449b43b09e5d603a87c408bc8652a6e8bb8139c
Gabriel Moreira <gabrieldevandrade49@gmail.com>
2025-02-05 20:18:30 -0300

    Adicionando codigo no gateway.js

---
+++ servidor/gateway.js
@@ -0,0 +1,28 @@
+const amqp = require('amqplib');
+
+async function startGateway() {
+  
+  try {
+    const connection = await amqp.connect('amqp://localhost');
+    const channel = await connection.createChannel();
+
+    const queues = ['fila_temperatura', 'fila_lampada', 'fila_tv'];
+
+    for (const queue of queues) {
+      await channel.assertQueue(queue, { durable: true });
+
+      console.log(`[*] Waiting for messages in queue: ${queue}`);
+      channel.consume(queue, (message) => {
+        if (message) {
+          const content = message.content.toString();
+          console.log(`Received from ${queue}: ${content}`);
+          channel.ack(message);
+        }
+      });
+    }
+  } catch (error) {
+    console.error('Error starting gateway:', error);
+  }
+}
+
+startGateway();
